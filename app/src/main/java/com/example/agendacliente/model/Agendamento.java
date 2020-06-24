d2914488522f7c01407d0dc4f036086630ba150b
Rodolpho Lopes <47788438+dolpholopes@users.noreply.github.com>
2020-06-24 18:10:13 -0300

    Bugs corrigidos

--- app/src/main/java/com/example/agendacliente/model/Agendamento.java
+++ app/src/main/java/com/example/agendacliente/model/Agendamento.java
@@ -5,20 +5,20 @@ public class Agendamento {
     private String nome;
     private String contato;
     private boolean wpp;
-    private boolean corte;
     private boolean barba;
+    private boolean corte;
     private boolean pigmentar;
     private boolean platinar;
 
     public Agendamento() {
     }
 
-    public Agendamento(String nome, String contato, boolean wpp, boolean corte, boolean barba, boolean pigmentar, boolean platinar) {
+    public Agendamento(String nome, String contato, boolean wpp, boolean barba, boolean corte, boolean pigmentar, boolean platinar) {
         this.nome = nome;
         this.contato = contato;
         this.wpp = wpp;
-        this.corte = corte;
         this.barba = barba;
+        this.corte = corte;
         this.pigmentar = pigmentar;
         this.platinar = platinar;
     }
@@ -47,20 +47,20 @@ public class Agendamento {
         this.wpp = wpp;
     }
 
-    public boolean isCorte() {
-        return corte;
-    }
-
-    public void setCorte(boolean corte) {
-        this.corte = corte;
-    }
-
     public boolean isBarba() {
         return barba;
     }
 
     public void setBarba(boolean barba) {
         this.barba = barba;
+    }
+
+    public boolean isCorte() {
+        return corte;
+    }
+
+    public void setCorte(boolean corte) {
+        this.corte = corte;
     }
 
     public boolean isPigmentar() {
